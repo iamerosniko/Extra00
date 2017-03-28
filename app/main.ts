182fc03cea1dafa76b9e9b7f45a51e1ad101d780
Eros Niko Alvarez <iamerosniko@yahoo.com>
2017-03-28 15:50:07 -0400

    up

--- app/main.ts
+++
@@ -1,6 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
-//import { enableProdMode } from '@angular/core'  // un comment this for PROD
-
-//enableProdMode(); // un comment this for PROD
-platformBrowserDynamic().bootstrapModule(AppModule);
