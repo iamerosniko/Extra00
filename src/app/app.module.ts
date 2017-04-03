fc55ce8fdc444bfd4dcdf102a83c62ba89f20a54
Alvarez <alverer@prd.manulifeusa.com>
2017-04-03 09:56:40 -0400

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 //ComponentModules
-import { SynchronizationModule } from './components/synchronization/sync.module';
+//import { SynchronizationModule } from './components/synchronization/sync.module';
 //routing
 import { AppRouting } from './app.routing';
 @NgModule({
-  imports:      [ BrowserModule , SynchronizationModule, AppRouting 
+  imports:      [ BrowserModule , 
+                  //SynchronizationModule, 
+                  AppRouting 
                 ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
