0b2a8a7c3c77c76cba444b6c35b0322cf684ea8a
Eros Niko Alvarez <iamerosniko@yahoo.com>
2017-04-04 15:49:49 -0400

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 //ComponentModules
-//import { SynchronizationModule } from './components/synchronization/sync.module';
 import { DISOverviewModule } from './components/03-disoverview/disoverview.module';
 import { DISToolsModule } from './components/05-dis-tools/distools.module';
 //routing
