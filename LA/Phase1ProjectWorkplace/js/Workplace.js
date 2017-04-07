59adc71e259effea39e53caf209b1df529d003c6
Luis Alfredo B Blanco V <blanco_luisalfredo@yahoo.com>
2017-04-07 11:44:20 -0400

    Update on Masterpage Header and Submenu > Submenu
    - Brand logo Added
    - Title Added
    - User details added
    - Collapsing submenu with a submenu

--- LA/Phase1ProjectWorkplace/js/Workplace.js
+++ LA/Phase1ProjectWorkplace/js/Workplace.js
@@ -1,27 +1,59 @@
- function ChangeToUp() {
+        function ChangeToUp1() {
                 document.getElementById("TriBtn1").className = "glyphicon glyphicon-triangle-top";
-                document.getElementById( "btn-1" ).setAttribute( "onClick", "javascript: ChangeToDown();" );
-
-                document.getElementById("TriBtn2").className = "glyphicon glyphicon-triangle-top";
-                document.getElementById( "btn-2" ).setAttribute( "onClick", "javascript: ChangeToDown();" );
-
-                document.getElementById("TriBtn3").className = "glyphicon glyphicon-triangle-top";
-                document.getElementById( "btn-3" ).setAttribute( "onClick", "javascript: ChangeToDown();" );
-
-                document.getElementById("TriBtn4").className = "glyphicon glyphicon-triangle-top";
-                document.getElementById( "btn-4" ).setAttribute( "onClick", "javascript: ChangeToDown();" );
+                document.getElementById( "btn-1" ).setAttribute( "onClick", "javascript: ChangeToDown1();" );
             }
 
-            function ChangeToDown() {
+            function ChangeToUp2() {
+                document.getElementById("TriBtn2").className = "glyphicon glyphicon-triangle-top";
+                document.getElementById( "btn-2" ).setAttribute( "onClick", "javascript: ChangeToDown2();" );
+            }
+
+            function ChangeToUp3() {
+                document.getElementById("TriBtn3").className = "glyphicon glyphicon-triangle-top";
+                document.getElementById( "btn-3" ).setAttribute( "onClick", "javascript: ChangeToDown3();" );
+            }
+
+            function ChangeToUp4() {
+                document.getElementById("TriBtn4").className = "glyphicon glyphicon-triangle-top";
+                document.getElementById( "btn-4" ).setAttribute( "onClick", "javascript: ChangeToDown4();" );
+            }
+           
+            function ChangeToDown1() {
                 document.getElementById("TriBtn1").className = "glyphicon glyphicon-triangle-bottom";
-                document.getElementById( "btn-1" ).setAttribute( "onClick", "javascript: ChangeToUp();" );
+                document.getElementById( "btn-1" ).setAttribute( "onClick", "javascript: ChangeToUp1();" );
+            }
 
+             function ChangeToDown2() {
                 document.getElementById("TriBtn2").className = "glyphicon glyphicon-triangle-bottom";
-                document.getElementById( "btn-2" ).setAttribute( "onClick", "javascript: ChangeToUp();" );
+                document.getElementById( "btn-2" ).setAttribute( "onClick", "javascript: ChangeToUp2();" );
+            }
 
+             function ChangeToDown3() {
                 document.getElementById("TriBtn3").className = "glyphicon glyphicon-triangle-bottom";
-                document.getElementById( "btn-3" ).setAttribute( "onClick", "javascript: ChangeToUp();" );
+                document.getElementById( "btn-3" ).setAttribute( "onClick", "javascript: ChangeToUp3();" );
+            }
 
+             function ChangeToDown4() {
                 document.getElementById("TriBtn4").className = "glyphicon glyphicon-triangle-bottom";
-                document.getElementById( "btn-4" ).setAttribute( "onClick", "javascript: ChangeToUp();" );
+                document.getElementById( "btn-4" ).setAttribute( "onClick", "javascript: ChangeToUp4();" );
+            }
+
+            function ChangeToUpSubMenu1() {
+                document.getElementById("TriBtn11").className = "glyphicon glyphicon-triangle-top";
+                document.getElementById( "btn-11" ).setAttribute( "onClick", "javascript: ChangeToDownSubMenu1();" );
+            }
+
+            function ChangeToDownSubMenu1() {
+                document.getElementById("TriBtn11").className = "glyphicon glyphicon-triangle-bottom";
+                document.getElementById( "btn-11" ).setAttribute( "onClick", "javascript: ChangeToUpSubMenu1();" );
+            }
+
+            function ChangeToUpSubMenu2() {
+                document.getElementById("TriBtn22").className = "glyphicon glyphicon-triangle-top";
+                document.getElementById( "btn-22" ).setAttribute( "onClick", "javascript: ChangeToDownSubMenu2();" );
+            }
+
+            function ChangeToDownSubMenu2() {
+                document.getElementById("TriBtn22").className = "glyphicon glyphicon-triangle-bottom";
+                document.getElementById( "btn-22" ).setAttribute( "onClick", "javascript: ChangeToUpSubMenu2();" );
             }
