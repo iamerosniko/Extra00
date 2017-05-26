aee7482fc429db3b1b41d5958cfbe14d30c73c20
Eros Niko Alvarez <iamerosniko@yahoo.com>
2017-05-26 11:36:06 -0400

    up

--- src/app/components/01-disoverview/programs.component.ts
+++ src/app/components/01-disoverview/programs.component.ts
@@ -7,18 +7,24 @@ import { ActivatedRoute,  Params, Router } from '@angular/router';
 export class ProgramsComponent implements OnInit { 
     slides: any[] = [];
     routeName: '';
+    breadcrumbs:any[]=[];
     public constructor(private route: ActivatedRoute,
         private router: Router) {
     }
 
     ngOnInit(){
+        console.log('ngoninit');
+        this.breadcrumbs=[];
+        this.breadcrumbs =['DIS Overview','Programs'];
         this.getRouteName();
         this.getImage();
     }
     getImage(){
+        console.log('ngoninit');
         this.addSlide(this.routeName+'.jpg');
     }
     getRouteName(){
+        console.log('ngoninit');
         this.route.params.subscribe(params => {
             this.routeName = params['id'];
             this.breadcrumbs.push(this.routeName);
@@ -31,5 +37,4 @@ export class ProgramsComponent implements OnInit {
         });
     }
 
-    breadcrumbs =['DIS Overview','Programs'];
 }
