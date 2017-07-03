eaea433d09fdaf3cfd318749fd99b4c6196b5a86
Eros Niko Alvarez <iamerosniko@yahoo.com>
2017-07-03 13:46:16 -0400

    up

--- src/app/components/02-teamoverview/team-org.component.ts
+++ src/app/components/02-teamoverview/team-org.component.ts
@@ -1,5 +1,6 @@
 import { Component,OnInit } from '@angular/core';
 import { ResourceService } from '../../services/resource.service';
+import { Resource } from '../../entities/resource';
 import { DomSanitizer } from '@angular/platform-browser';
 @Component({
     moduleId: module.id,
@@ -8,7 +9,8 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class TeamOrgComponent implements OnInit {
     slides: any[] = [];
     content:any;
-    path:string =''
+    path:string ='';
+    path2:Resource;
     public constructor
     (private resourceService : ResourceService
     ,public sanitizer: DomSanitizer) {
@@ -20,8 +22,12 @@ export class TeamOrgComponent implements OnInit {
             .then(res => {
                 this.path=res.ResourcePath
             });
+          this.sample();
     }
 
+    async sample(){
+        this.path2= await this.resourceService.getResourcePath2('Team Org')
+    }
     // addSlide(filename:string): void {
     //     this.slides.push({
     //         image: `resources/workplace/02TEAM OVERVIEW/Gian Karlo Villaluz/${ filename }`
